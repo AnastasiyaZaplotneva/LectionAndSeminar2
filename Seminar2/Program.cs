9351985d1d180ac42c9b0cbebdff2038c163a75d
Anastasiya Z <nastya_tim2201@mail.ru>
2022-10-17 16:10:38 +0300

    домашка для второго семинара

---
+++ Seminar2/Program.cs
@@ -0,0 +1,63 @@
+﻿// поиск третьей цифры любого числа
+// Console.WriteLine("Введите число ");
+// int number = int.Parse(Console.ReadLine());
+
+// int count = 0;
+// int num = number;
+
+// while(num > 0)
+// {
+//     num = num / 10;
+//     count++;
+// }
+
+// if(count < 3)
+// {
+//     Console.WriteLine("У числа нет третьей цифры");
+// }
+// else
+// {
+// int[] array = new int[count];
+
+// int index = array.Length - 1;
+
+// while(number > 0)
+// {
+//     array[index] = number%10;
+//     number = number/10;
+//     index--;
+// }
+// Console.WriteLine(array[2]);
+// }
+
+//поиск второй цифры трехзначного числа
+// Console.WriteLine("Введите трехзначное число ");
+// int number = int.Parse(Console.ReadLine());
+
+// int[] array = new int[3];
+
+// int index = array.Length - 1;
+
+// while(number > 0)
+// {
+//      array[index] = number%10;
+//      number = number/10;
+//      index--;
+// }
+// Console.WriteLine(array[1]);
+
+//день недели выходной или будний
+
+// string[] days = {"нулевого дня нет", "будний день", "будний день", "будний день", "будний день", "будний день", "выходной день", "выходной день"};
+
+// Console.WriteLine("Введите число от 1 до 7");
+// int number = int.Parse(Console.ReadLine());
+
+// if(number > 7)
+// {
+//     Console.WriteLine("Вы ввели неверное число");
+// }
+// else
+// {
+//     Console.WriteLine(days[number]);
+// }
